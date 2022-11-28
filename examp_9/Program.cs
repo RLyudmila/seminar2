5587a69179ba48220c53957803e09a872863715c
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2022-11-28 22:18:30 +0300

    number day of the week

---
+++ examp_9/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да   1 -> нет
+
+Random random = new Random();
+int numberday = random.Next(0,8);
+Console.WriteLine(numberday);
+
+if (numberday < 1 | numberday >7)
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+else
+{
+    if (numberday == 6 | numberday == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
