173c6c41379d58584db1725724334f75f071b9b6
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2022-11-28 11:32:09 +0300

    the second number

--- examp_7/Program.cs
+++ examp_7/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+Random random = new Random();
+int number = random.Next(100,999);
+Console.WriteLine(number);
+
+int twonumber= number/10;
+int secondnumber = twonumber % 10;
+
+Console.WriteLine(secondnumber);
