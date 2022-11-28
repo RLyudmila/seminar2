943042a8b9f7bf20351f59d48cff170af4fb30d7
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2022-11-28 21:33:01 +0300

    task two

---
+++ examp_8/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Random random = new Random();
+int number = random.Next(10,100000);
+Console.WriteLine(number);
+
+if (number <= 99 & number >= 10)
+{
+   Console.WriteLine("Двухзначное число");
+}
+
+else
+{
+if (number > 100 & number <= 999)
+{
+int twonumber= number/10;
+int secondnumber = twonumber % 10;
+Console.WriteLine(secondnumber);
+}
+  else
+  {
+   if (number >= 1000)
+   {
+      
+       while (number > 999)
+       {
+         number = number / 10;
+       }
+       int result = number % 10;
+       Console.WriteLine(result);
+
+
+   }
+  }  
+}
+ 
+   
